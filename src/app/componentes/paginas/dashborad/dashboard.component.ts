7788fc6fd327c208b668722b2d728cc8f83fb396
luisito666 <luispenagos91@gmail.com>
2017-11-06 01:55:20 -0500

    realizando primer build exitoso en travis

--- src/app/componentes/paginas/dashborad/dashboard.component.ts
+++ src/app/componentes/paginas/dashborad/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService, TitleService, AuthService } from '../../../servicios/servicios.module';
 import { Data } from '../../../interfaces/data.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,7 +20,8 @@ export class DashboardComponent implements OnInit {
   constructor(
     private _app:AppService,
     private _titulo:TitleService,
-    private auth:AuthService
+    private _router:Router,
+    public auth:AuthService
   ){
     this._titulo.setTitulo('Metin2 '+this._titulo.servername+' - Panel usuario');
     if(localStorage.getItem('jwt')){
@@ -27,6 +29,7 @@ export class DashboardComponent implements OnInit {
             .subscribe(respuesta => {
                 if(respuesta.status == "expiro"){
                     localStorage.removeItem('jwt');
+                    this._router.navigate(['/login'])
                 } else {
                   this.data = respuesta;
                 }
